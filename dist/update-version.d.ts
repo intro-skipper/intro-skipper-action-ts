9616d7515069e281fdeb94a7d8dbec1bc39dc735
Kilian von Pflugk <github@jumoog.io>
2025-05-16 13:04:09 +0200

    regen dist

--- dist/update-version.d.ts
+++
@@ -1,9 +0,0 @@
-import * as core from '@actions/core';
-import fs from 'fs';
-export declare function incrementVersion(version: string): string;
-export declare function updateVersion(csprojPath?: string, fileSystem?: typeof fs, logger?: typeof core): Promise<void>;
-export declare function updateVersionsInData(data: string): {
-    updatedData: string;
-    newAssemblyVersion: string;
-    newFileVersion: string;
-};
