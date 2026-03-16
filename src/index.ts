f84aff390d5222e6f6e213765a50b4b8897cb97c
Kilian von Pflugk <github@jumoog.io>
2026-03-16 23:17:37 +0100

    add addSpdxHeaders

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as core from '@actions/core'
  */
 import { updateManifest } from './validate-and-update-manifest.js'
 import { updateVersion } from './update-version.js'
+import { addSpdxHeaders } from './add-spdx-headers.js'
 
 const taskType = core.getInput('task-type')
 
@@ -12,6 +13,8 @@ if (taskType === 'updateManifest') {
   updateManifest()
 } else if (taskType === 'updateVersion') {
   updateVersion()
+} else if (taskType === 'addSpdxHeaders') {
+  await addSpdxHeaders()
 } else {
   core.setFailed(`Invalid task type: ${taskType}`)
 }
